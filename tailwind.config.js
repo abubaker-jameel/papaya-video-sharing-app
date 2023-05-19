565ec65e15a87878defce4b3f808a625da374a34
abubaker <codecookies958@gmail.com>
2023-05-20 01:26:26 +0500

    setup colors for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primaryWhite: "#FFFFFF",
+      darkPurple: "#48319D",
+      neonPurple: "#8015A7",
+      deepPurple: "#613690",
+      red: "#ED695E",
+      yellow: "#F4BF4F",
+      green: "#61C554",
+      purple: "#8322FF",
+      blue: "#22D7FF",
+      dark01: "#2E335A",
+      dark02: "#454B79",
+      dark03: "#0C1033",
+      dark04: "#1D234D",
     },
+    extend: {},
   },
   plugins: [],
-}
+};
